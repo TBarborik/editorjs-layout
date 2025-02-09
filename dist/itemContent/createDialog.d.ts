ed9321800fbc6645e0b2065e763b22f680c13b76
Tom Barbořík <barborik.tom25@gmail.com>
2025-02-09 18:16:40 +0100

    dist

---
+++ dist/itemContent/createDialog.d.ts
@@ -0,0 +1,11 @@
+import type { OutputData } from "@editorjs/editorjs";
+import type { LayoutBlockToolConfig } from "../LayoutBlockTool";
+import "bootstrap/js/dist/modal";
+export declare const createDialog: ({ EditorJS, data, editorJSConfig, onClose }: {
+    EditorJS: LayoutBlockToolConfig["EditorJS"];
+    data: OutputData;
+    editorJSConfig: LayoutBlockToolConfig["editorJSConfig"];
+    onClose?: ((event: {
+        editorJSData: OutputData;
+    }) => void) | undefined;
+}) => JQuery<HTMLDivElement>;
